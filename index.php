71f8a6b020d9dc2ddc251c7c17f64a9d2564109e
Jethromwantok <130243517+Jethromwantok@users.noreply.github.com>
2025-04-04 12:29:35 +0100

    His

--- index.php
+++ index.php
@@ -7,7 +7,9 @@
 </head>
 <body>
 <!-- text, password, email, number, date, time, checkbox, radio, file, submit, reset, button, hidden, These are all input types under  -->
-    <form action="index.php" method="get"> <!--The action attribute is used to specify where the form data should be sent. The method attribute specifies how to send form-data. The form-data is sent to the page specified in the action attribute. The form-data can be sent as URL variables (with method="get") or as HTTP post transaction (with method="post").-->
+    <form action="index.php" method="post"> <!--The action attribute is used to specify where the form data should be sent. The method attribute specifies how to send form-data. The form-data is sent to the page specified in the action attribute. The form-data can be sent as URL variables (with method="get") or as HTTP post transaction (with method="post").-->
+    <!-- Get isnt safe
+     use post-->
         <label> Username:</label>
         <input type="Username" placeholder="Enter Username" name="username">
         <br>
@@ -23,12 +25,20 @@
 
 
 <?php
+
+    $num = 10;
+    $num2 = 20;
+
+    $add = $num + $num2;
+    $mul = $num * $num2;
+    $sub = $num - $num2;
+    $div = $num2/$num;
     // echo"Hello World! <br>"; /*The '<>' is for new line */
     // echo "I'm new to this";
 
     // $name1 = " Jethro";
     // $name2 = " John";
-    // echo "Hello {$name1} {$name2} is now a PHP developer <br>";
+    // echo "Hello {$name1} {$name2} is now a PHP developer <br>"; Learning to concatenate strings
 
 	// // PHP uses implicit declaration of variables. This means that you don't have to declare a variable before using it. You can just assign a value to a variable and PHP will automatically declare it for you.
 
@@ -56,7 +66,30 @@
 
     //logical operators
     //&&(And) ||(or) !(Not)
+
+    echo "{$_POST["username"]} <br>"//This is to concatenate theresult of the GET with other text
+    /*GET
+     nor secure
+     char limit
+     bookmarking specifics is possible
+     GET requests can be cached
+     Better for search pages
+
+     POST
+     secure
+     no char limit
+     bookmarking specifics is not possible
+     POST requests are not cached
+     Better for forms and sensitive data
     
+
+    Some math operations, round, floor, ceil, pow, abs
+     */ 
+    //Usage
+
+  
+
+
 
 
 ?>
